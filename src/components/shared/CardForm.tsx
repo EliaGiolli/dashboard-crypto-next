1ce9d79136193dd29efa065a20fb009bf678cd25
Elia Giolli <eliagiolli22@gmail.com>
2025-10-21 10:41:59 +0200

    form validated with react-hook-form e zod. Homepage done

--- src/components/shared/CardForm.tsx
+++ src/components/shared/CardForm.tsx
@@ -14,13 +14,13 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 //Internal imports
-import Link from "next/link";
+import { useRouter } from "next/router"
 import { type FormSchema, formSchema } from "../../schemas/formSchema";
 
 //External libs
 import { useForm } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
-
+import { motion } from 'motion/react';
 
 export function CardForm() {
 
@@ -31,28 +31,47 @@ export function CardForm() {
     } = useForm<FormSchema>({ resolver: zodResolver(formSchema) }
   );
 
+  const onSubmit = () => {
+    console.log('form submitted')
+  }
+
+  const MotionButton = motion(Button);
+  const router = useRouter();
+
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
+        <CardTitle>Accedi al tuo account</CardTitle>
         <CardDescription>
-          Enter your email below to login to your account
+          Inserisci la tua email e la tua password per accedere
         </CardDescription>
         <CardAction>
-          <Button variant="link">Sign Up</Button>
+          <Button variant="link">Accedi</Button>
         </CardAction>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
                 type="email"
+                {...register("email", {
+                  required: "il campo è obbligatorio",
+                  minLength: { value: 3, message: "Deve contenere almeno 3 caratteri" },
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                    message: "Inserisci un'email valida"
+                  }
+                })}
                 placeholder="m@example.com"
-                required
               />
+              { errors.email && (
+                <p id="error-email" role="alert" className="bg-red-100 text-red-600 text-2xl p-2">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
@@ -61,21 +80,39 @@ export function CardForm() {
                   href="#"
                   className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                 >
-                  Forgot your password?
+                  Hai dimenticato la tua password?
                 </a>
               </div>
-              <Input id="password" type="password" required />
+              <Input 
+                id="password" 
+                type="password" 
+                {...register("password", {
+                    required: "il campo è obbligatorio",
+                    minLength: { value: 8, message: "il campo non può essere vuoto" },
+                    pattern: {
+                      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                      message: "Inserisci una password valida"
+                    }
+              })} />
+              { errors.password && (
+                <p id="error-password" role="alert" className="bg-red-100 text-red-600 text-2xl p-2">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
           </div>
         </form>
       </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
+      <CardFooter className="w-full">
+        <MotionButton 
+          type="submit" 
+          onClick={() => router.push('/login')}
+          className="w-1/2"
+          whileHover={{ scale: 1 }}
+          whileFocus={{ scale: 1 }}
+          >
           Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
+        </MotionButton>
       </CardFooter>
     </Card>
   )
