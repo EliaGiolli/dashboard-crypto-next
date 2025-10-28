0ff2fceba605595ef2bc7b4c63db9deaa02eab2c
Elia Giolli <eliagiolli22@gmail.com>
2025-10-28 14:52:53 +0100

    completed the serverless backend logic with db connection, models, services and the route.ts that acts as controller

--- src/lib/server.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI!)
-    .then(() => console.log("connesso a MongoDB"))
-    .catch((err) => console.error(err))
